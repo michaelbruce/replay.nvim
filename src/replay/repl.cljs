bb4926796be1f6ca9c90dfcf0ca6b4bc672158c5
Michael Bruce <mike@bearmetalcoding.com>
2017-02-20 11:41:53 +0000

    Using commandSync command to create a vim function

--- src/replay/repl.cljs
+++ src/replay/repl.cljs
@@ -3,8 +3,11 @@
 
 (nodejs/enable-util-print!)
 
-(defn test-function [nvim]
+;; current-buffer is req'd by .commandSync
+(defn test-function [nvim current-buffer]
   (.command nvim "echo \"The eagle has landed!\""))
 
 (defn -main []
-  (.command js/plugin "ReplayFunction" #js {:nargs 0} test-function))
+  ;; (.commandSync js/plugin "ReplayFunction" #js {:nargs 0} test-function)
+  (.commandSync js/plugin "ReplayFunction" test-function)
+  )
